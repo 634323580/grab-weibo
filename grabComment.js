75b2af91bf97f27c9854f1f60bb75b69eebe914a
ChenZhiPeng <634323580@qq.com>
2019-03-14 13:47:10 +0800

    随便写写

--- grabComment.js
+++ grabComment.js
@@ -3,107 +3,127 @@ const axios = require('axios-https-proxy-fix'),
   mysql = require('mysql2/promise');
 
 
-  (async () => {
-    // 创建数据库连接
-    const connection = await mysql.createConnection({
-      host: '192.168.31.223',
-      port: '6610',
-      user: 'qph_b2c',
-      password:'zhaoyl(1181*%P)',
-      database: 'zhipeng',
-      charset:'utf8',
-    });
-    // 设置代理ip
-    let proxy = {
-      host: '',
-      port: '',
-    }
-    let length = 0
-    const [rows,fields] = await connection.execute('select * from list');
-    // 抓取的微博数据
-    grabKeyData = rows;
-    for(let i=0; i<grabKeyData.length; i++) {
-        await start(grabKeyData[i].mid)
-        console.log('当前进度' + (i+1 / grabKeyData.length * 100).toFixed(2) + '%')
-    }
-    connection.end();
-    /**
-     * @description 开始抓取
-     * @param uid 微博id
-     */
-    async function start(mid) {
-      let url = 'https://weibo.com/aj/v6/comment/big?ajwvr=6&id='+mid+'&from=singleWeiBo&__rnd=1551851364327'
-      async function cb(url) {
-        // 抓取评论
-        let _html = await getComments(url)
-        // 当前页的一级评论
-        let _commentDom = $(_html).children('.list_ul').children('.list_li.S_line1')
-        // 获取下一页评论需要的参数
-        let _actionData = $(_html).find('div[node-type="comment_loading"]').attr('action-data') || $(_html).find('a[action-type="click_more_comment"]').attr('action-data')
-        _commentDom.each(async (index, commentItem) => {
-          commentItem = $(commentItem)
-          // 头像
-          let _avator = commentItem.find('.WB_face').find('img').attr('src')
-          // 昵称
-          let _name = commentItem.find('.list_con>.WB_text>a').eq(0).text()
-            // 评论内容
-          let _content = $.load(commentItem.find('.list_con>.WB_text').html(), {decodeEntities: false})
-          // 删除a标签
-           _content('body a').remove()
-           _content = _content('body').html().trim()
-          // 将数据插入数据库
-          await connection.query('insert into comments set ?',{
-            mid: mid,
-            avator: _avator,
-            name: _name,
-            content: _content
-          })
-          length++
+(async () => {
+  // 创建数据库连接
+  const connection = await mysql.createConnection({
+    host: '192.168.31.223',
+    port: '6610',
+    user: 'qph_b2c',
+    password: 'zhaoyl(1181*%P)',
+    database: 'zhipeng',
+    charset: 'utf8',
+  });
+  // 设置代理ip
+  let proxy = {
+    host: '',
+    port: '',
+  }
+  let length = 0
+  const [rows, fields] = await connection.execute('select * from list');
+  // 抓取的微博数据
+  grabKeyData = rows;
+  for (let i = 0; i < grabKeyData.length; i++) {
+    await start(grabKeyData[i].mid)
+    console.log('当前进度' + ((i + 1 ) / grabKeyData.length * 100).toFixed(2) + '%')
+  }
+  connection.end();
+  /**
+   * @description 开始抓取
+   * @param uid 微博id
+   */
+  async function start(mid) {
+    let url = 'https://weibo.com/aj/v6/comment/big?ajwvr=6&id=' + mid + '&from=singleWeiBo&__rnd=1551851364327'
+    async function cb(url) {
+      // 抓取评论
+      let _html = await getComments(url)
+      let {actionData, list} = analysisHtml(_html)
+      list.forEach(async item => {
+        // 将数据插入数据库
+        await connection.query('insert into comments set ?', {
+          mid: mid,
+          comment_id: item.comment_id,
+          avator: item.avator,
+          name: item.name,
+          content: item.content
         })
-        console.log('已插入'+length+'条数据')
-        if(_actionData) {
-          await cb('https://weibo.com/aj/v6/comment/big?ajwvr=6&'+_actionData+'&from=singleWeiBo&__rnd=1551851364327')
-        }
-      }
-      await cb(url)
-    }
-      /**
-     * @description 获取评论，自动切换代理
-     * @param url 接口地址
-     */
-    async function getComments(url) {
-      let _option = {
-        headers: {
-          Cookie: 'Ugrow-G0=57484c7c1ded49566c905773d5d00f82; login_sid_t=264d7a3b7802fa881aa6803a2a3ceb4e; cross_origin_proto=SSL; _s_tentry=passport.weibo.com; wb_view_log=1920*10801; Apache=7892657198804.198.1552355975421; SINAGLOBAL=7892657198804.198.1552355975421; ULV=1552355975427:1:1:1:7892657198804.198.1552355975421:; WBtopGlobal_register_version=ae9a9ec008078a68; SCF=Avt6CuTc8f5ibnk_xMIGBUniVSnlPtQvnf6VGPob_22AX9xL0OTBxfmOlnQLLqVO6BUUfFKzaiI2TdRmitgKxf8.; SUB=_2A25xg2L5DeRhGeVP41ET8CvJzziIHXVS-dMxrDV8PUNbmtBeLVnTkW9NTMLHgkLP0jMCrbXMvWawwqdkVrOWng2N; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF4I43carcJRI5MKE2C1Hu45JpX5K2hUgL.Foep1heEeh-fShB2dJLoIEqLxKnLBoMLB-qLxK-L1-eLBKnLxKnL1hBL1-2LxKBLBonL12zEentt; SUHB=09P9Cp1w-hR9ky; ALF=1552960809; SSOLoginState=1552356009; un=18320326435; wvr=6; YF-Page-G0=b98b45d9bba85e843a07e69c0880151a; wb_view_log_3183205544=1920*10801; YF-V5-G0=a5a6106293f9aeef5e34a2e71f04fae4; webim_unReadCount=%7B%22time%22%3A1552374525481%2C%22dm_pub_total%22%3A5%2C%22chat_group_pc%22%3A0%2C%22allcountNum%22%3A32%2C%22msgbox%22%3A0%7D'
-        },
-        // 设置超时10秒
-        timeout: 30000
-      }
-      // 是否设置了代理
-      if(proxy.host) {
-        _option['proxy'] = {
-          host: proxy.host,
-          port: proxy.port,
-        }
-      }
-      try {
-        let _res = await axios.get(url, _option)
-        return new Promise(resolve => {
-          resolve(_res.data.data.html)
-        })
-      } catch (error) {
-        console.log('正在切换ip...')
-        let _proxy = await axios.get('http://webapi.http.zhimacangku.com/getip?num=1&type=2&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=')
-
-        if(_proxy.data.code === 0) {
-          proxy = {
-            host: _proxy.data.data[0].ip,
-            port: _proxy.data.data[0].port
-          }
-          console.log('切换ip成功, host:'+proxy.host+';-port:'+proxy.port)
-        }
-        return await getComments(url)
+        length++
+      })
+      console.log('已插入' + length + '条数据')
+      if (actionData) {
+        await cb('https://weibo.com/aj/v6/comment/big?ajwvr=6&' + actionData + '&from=singleWeiBo&__rnd=1551851364327')
       }
     }
-  })()
+    await cb(url)
+  }
+  /**
+   * @description 解析html
+   * @param html html字符串
+   * @return {String} actionData=>获取下一页所需参数，如果为空则没有下一页
+   * @return {Array} list=>评论数据
+   */
+  function analysisHtml(html){
+    let list = []
+    // 当前页的一级评论
+    let commentDom = $(html).children('.list_ul').children('.list_li.S_line1')
+    // 获取下一页评论需要的参数
+    let actionData = $(html).find('div[node-type="comment_loading"]').attr('action-data') || $(html).find('a[action-type="click_more_comment"]').attr('action-data')
+    commentDom.each(async (index, commentItem) => {
+      commentItem = $(commentItem)
+      // 评论内容
+      let content = $.load(commentItem.find('.list_con>.WB_text').html(), {
+        decodeEntities: false
+      })
+      // 删除a标签
+      content('body a').remove()
+      content = content('body').html().trim()
+      list.push({
+        comment_id: commentDom.attr('comment_id'),
+        avator: commentItem.find('.WB_face').find('img').attr('src'),
+        name: commentItem.find('.list_con>.WB_text>a').eq(0).text(),
+        content: content.substr(1)
+      })
+    })
+    return {
+      actionData,
+      list
+    }
+  }
+  /**
+   * @description 获取评论，自动切换代理
+   * @param url 接口地址
+   */
+  async function getComments(url) {
+    let _option = {
+      headers: {
+        Cookie: 'Ugrow-G0=9642b0b34b4c0d569ed7a372f8823a8e; login_sid_t=43ae95196399c13bdcee8acb979a8186; cross_origin_proto=SSL; YF-V5-G0=9717632f62066ddd544bf04f733ad50a; WBStorage=39aa940fb6ef309a|undefined; _s_tentry=passport.weibo.com; wb_view_log=1920*10801; Apache=8956404726944.105.1552529497257; SINAGLOBAL=8956404726944.105.1552529497257; ULV=1552529497271:1:1:1:8956404726944.105.1552529497257:; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WF4I43carcJRI5MKE2C1Hu45JpX5K2hUgL.Foep1heEeh-fShB2dJLoIEqLxKnLBoMLB-qLxK-L1-eLBKnLxKnL1hBL1-2LxKBLBonL12zEentt; SSOLoginState=1552529505; ALF=1584065571; SCF=Asn_RkWnRnTkS95UEL78CR-Zogi1iLt_gUNJLPlnd1J6oSi1Bq75aoUhqWq4wLE7EpV_NPch0eaQOZCqsT1lFQs.; SUB=_2A25xjcgsDeRhGeVP41ET8CvJzziIHXVS-r7krDV8PUNbmtBeLXHCkW9NTMLHgkdErknRtkNhleOikTfuGo0C9xb0; SUHB=02MvVc-iLM6pO6; un=18320326435; wvr=6; YF-Page-G0=c704b1074605efc315869695a91e5996; wb_view_log_3183205544=1920*10801; webim_unReadCount=%7B%22time%22%3A1552529555784%2C%22dm_pub_total%22%3A5%2C%22chat_group_pc%22%3A0%2C%22allcountNum%22%3A40%2C%22msgbox%22%3A0%7D'
+      },
+      // 设置超时
+      timeout: 30000
+    }
+    // 是否设置了代理
+    if (proxy.host) {
+      _option['proxy'] = {
+        host: proxy.host,
+        port: proxy.port,
+      }
+    }
+    try {
+      let _res = await axios.get(url, _option)
+      return new Promise(resolve => {
+        resolve(_res.data.data.html)
+      })
+    } catch (error) {
+      console.log('正在切换ip...')
+      let _proxy = await axios.get('http://webapi.http.zhimacangku.com/getip?num=1&type=2&pro=&city=0&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=1&sb=0&pb=4&mr=1&regions=')
 
+      if (_proxy.data.code === 0) {
+        proxy = {
+          host: _proxy.data.data[0].ip,
+          port: _proxy.data.data[0].port
+        }
+        console.log('切换ip成功, host:' + proxy.host + ';-port:' + proxy.port)
+      }
+      return await getComments(url)
+    }
+  }
+})()
--- grabKey.js
+++ grabKey.js
@@ -2,18 +2,18 @@ const qs = require('querystringify'),
   puppeteer = require('puppeteer'),
   mysql = require('mysql2');
 
-  // 创建数据库连接
-  const connection = mysql.createConnection({
-    host: '192.168.31.223',
-    port: '6610',
-    user: 'qph_b2c',
-    password:'zhaoyl(1181*%P)',
-    database: 'zhipeng',
-    charset:'utf8',
-  });
-  connection.connect();
+// 创建数据库连接
+const connection = mysql.createConnection({
+  host: '192.168.31.223',
+  port: '6610',
+  user: 'qph_b2c',
+  password: 'zhaoyl(1181*%P)',
+  database: 'zhipeng',
+  charset: 'utf8',
+});
+connection.connect();
 
-(async function(){
+(async function () {
   /**
    * @description 创建新页面
    * @param url 页面地址
@@ -21,14 +21,14 @@ const qs = require('querystringify'),
   async function newPage(url) {
     const page = (await browser.pages())[0];
     await page.setViewport({
-        width:1360,
-        height:768
+      width: 1360,
+      height: 768
     })
     await page.goto(url, {
-        timeout: 0
+      timeout: 0
     });
     await page.addScriptTag({
-        url: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"
+      url: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"
     });
     return page
   }
@@ -42,15 +42,15 @@ const qs = require('querystringify'),
     let login = await newPage('https://www.weibo.com')
     // 等待浏览器加载完毕
     await login.waitForNavigation({
-        waitUntil: ["load"],
-        timeout: 0
+      waitUntil: ["load"],
+      timeout: 0
     });
 
     // 模拟输入用户名
     await login.waitForSelector("#loginname");
     await login.focus("#loginname");
     await login.keyboard.type(user, {
-        delay: 10
+      delay: 10
     });
 
     // 模拟输入密码
@@ -61,46 +61,47 @@ const qs = require('querystringify'),
     await login.focus("input[name=password]");
     // 键盘输入密码，每次按键间隔10ms
     await login.keyboard.type(password, {
-        delay: 10
+      delay: 10
     });
     // 模拟点击登录
     console.log("登录中...");
     await login.click("a[action-type=btn_submit]", {
-        delay: 500
+      delay: 500
     });
     await login.waitForNavigation({
-        waitUntil: ["load"],
-        timeout: 0
+      waitUntil: ["load"],
+      timeout: 0
     });
     console.log("登录成功\n\n");
   }
 
   /**
    * @description 通过搜索抓取关键字微博
-   * @param keyword 关键字
+   * @param option 选项
    */
-  async function geabKey(keyword) {
-    let page = await newPage("https://s.weibo.com/weibo?q="+keyword+"&Refer=index"),
-    // 总页数
-    totalPage,
-    // 当前页数
-    currentPage = 1
+  async function geabKey(option) {
+    let query = decodeURIComponent(qs.stringify(option)),
+      page = await newPage("https://s.weibo.com/weibo?" + query + "&Refer=index"),
+      // 总页数
+      totalPage,
+      // 当前页数
+      currentPage = 1
     // 获取总页数
     totalPage = await page.evaluate(() => {
-        return new Promise(resolve => {
-          resolve($('.s-scroll>li:last-child>a').attr('href').split('?')[1])
-        })
+      return new Promise(resolve => {
+        resolve($('.s-scroll>li:last-child>a').attr('href').split('?')[1])
+      })
     });
     totalPage = qs.parse(totalPage).page
-    let start = async function() {
-      console.log('正在抓取第'+currentPage+'页')
+    let start = async function () {
+      console.log('正在抓取第' + currentPage + '页')
       let pageList = await page.evaluate(async () => {
         let _list = $('#pl_feedlist_index .card-wrap')
         let _keyList = []
         _list.each((index, item) => {
           // 去除转发的微博
           $(item).find('.card-comment').length && $(item).find('.card-comment').remove()
-          let _content = $(item).find('p[node-type="feed_list_content_full"]').length?$(item).find('p[node-type="feed_list_content_full"]'):$(item).find('p[node-type="feed_list_content"]')
+          let _content = $(item).find('p[node-type="feed_list_content_full"]').length ? $(item).find('p[node-type="feed_list_content_full"]') : $(item).find('p[node-type="feed_list_content"]')
           _content.find('a').length && _content.find('a').remove()
           _content = String(_content.html()).replace(/<(?!img).*?>/g, '').trim()
           let _picList = []
@@ -109,7 +110,7 @@ const qs = require('querystringify'),
             _picList.push($(item).find('img').attr('src'))
           })
           // 存在此元素，判断为微博
-          if($(item).find('.card-act').find('a[action-type=feed_list_comment]')[0]) {
+          if ($(item).find('.card-act').find('a[action-type=feed_list_comment]')[0]) {
             _keyList.push({
               mid: $(item).attr('mid'),
               // 用户名
@@ -129,22 +130,22 @@ const qs = require('querystringify'),
       })
       //把对象的转成纯数组
       let values = [];
-      pageList.forEach(function(n, i){
-          var _arr = [];
-          for(var m in n){
-              _arr.push(n[m]);
-          }
-          values.push(_arr);
+      pageList.forEach(function (n, i) {
+        var _arr = [];
+        for (var m in n) {
+          _arr.push(n[m]);
+        }
+        values.push(_arr);
       })
       // 保存
-      connection.query('INSERT INTO list(`mid`,`name`,`avator`,`content`,`piclist`) VALUES ?', [values], function(err, result) {
-          if (err) throw err;
-          console.log('插入'+pageList.length+'条数据')
+      connection.query('INSERT INTO list(`mid`,`name`,`avator`,`content`,`piclist`) VALUES ?', [values], function (err, result) {
+        if (err) throw err;
+        console.log('插入' + pageList.length + '条数据')
       });
       currentPage++
-      if(currentPage <= totalPage) {
+      if (currentPage <= totalPage) {
         // 跳转到下一页
-        await page.goto("https://s.weibo.com/weibo?q="+keyword+"&Refer=index&page="+currentPage , {
+        await page.goto("https://s.weibo.com/weibo?" + query + "&Refer=index&page=" + currentPage, {
           timeout: 0
         });
         // 插入jq
@@ -152,7 +153,7 @@ const qs = require('querystringify'),
           url: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"
         });
         // 再次抓取
-        await start(keyword)
+        await start()
       }
     }
     await start()
@@ -166,7 +167,10 @@ const qs = require('querystringify'),
   // 自动登录
   await automaticLogin('18320326435', 'KOFOX520')
   // 开始抓取关键字微博
-  await geabKey('面膜')
+  await geabKey({
+    q: '面膜',
+    timescope: 'custom:2019-03-01:2019-03-05'
+  })
 
   browser.close()
   connection.end();
